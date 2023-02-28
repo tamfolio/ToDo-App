78605d1a76c830c7f7886d6afaa958f953e4aa0a
Tamilore <tamiloreolaniyi@gmail.com>
2023-02-28 11:18:45 +0100

    flatlist

--- todo/App.js
+++ todo/App.js
@@ -2,13 +2,23 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text: 'Liverpool win', key: "1"},
+    {text: 'Chelsea win', key: "2"},
+    {text: 'Mancity win', key: "3"},
+  ])
   return (
     <View style={styles.container}>
       {/* HEADER */}
       <View style={styles.content}>
         {/* TODO Form */}
         <View style={styles.list}>
-            <FlatList/>
+            <FlatList
+              data={todos}
+              renderItem={({item}) => (
+                <Text>{item.text}</Text>
+            )}
+            />
         </View>
       </View>
     </View>
