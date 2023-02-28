d60f0f256d9f11d6b08ad528808d522c65a4d147
Tamilore <tamiloreolaniyi@gmail.com>
2023-02-28 11:48:20 +0100

    item styling

--- todo/components/todoitem.js
+++ todo/components/todoitem.js
@@ -4,12 +4,19 @@ import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 export default function TodoItem({item}) {
     return (
       <TouchableOpacity>
-         <Text>{item.text}</Text>
+         <Text style={styles.item}>{item.text}</Text>
       </TouchableOpacity>
     )
 }
 
 
 const styles = StyleSheet.create({
-   
+   item: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: "#bbb",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 10
+   }
 })
