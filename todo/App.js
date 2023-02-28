c23591295caf95a019c80f9b471c8ae52b12a7e0
Tamilore <tamiloreolaniyi@gmail.com>
2023-02-28 11:12:03 +0100

    to do

--- todo/App.js
+++ todo/App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* HEADER */}
+      <View style={styles.content}>
+        {/* TODO Form */}
+        <View style={styles.list}>
+            <FlatList/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +19,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
