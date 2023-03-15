d46ddfe2bf54ea10aa2c0ddb063b524ee24aad42
Tamilore <tamiloreolaniyi@gmail.com>
2023-03-15 15:39:34 +0100

    sandbox

---
+++ todo/components/sandbox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SandBox(){
+    return(
+        <Text>Sandbox</Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
