b1a4dcbdfd618f1b40c71a4bd386560488c53737
Tamilore <tamiloreolaniyi@gmail.com>
2023-03-15 15:23:20 +0100

    texts

--- todo/App.js
+++ todo/App.js
@@ -18,12 +18,15 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
+    if(text.length > 3){
+      setTodos((prevTodos) => {
         return[
           {text: text, key: Math.random().toString()},
           ...prevTodos
         ]
     })
+    }
+    
   }
   return (
     <View style={styles.container}>
