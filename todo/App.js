70caf7e03def70c23e7689b5f11030734acab3fc
Tamilore <tamiloreolaniyi@gmail.com>
2023-03-15 17:03:46 +0100

    todo flex

--- todo/App.js
+++ todo/App.js
@@ -64,9 +64,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   content: {
-    padding: 40
+    padding: 40,
+    flex: 1
   },
   list: {
-    marginTop: 20
+    marginTop: 20,
+    flex: 1
   }
 });
